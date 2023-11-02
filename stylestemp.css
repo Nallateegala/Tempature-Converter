71ef01a8b29812d0789a6a1d93e985273af75b10
Nallateegala <149683602+Nallateegala@users.noreply.github.com>
2023-11-02 18:02:39 +0530

    Add files via upload

---
+++ stylestemp.css
@@ -0,0 +1,56 @@
+body{
+	  font-family:Arial,Calibri;
+	  background:white;
+	margin:0;
+	padding:0;
+	display:flex;
+	justify-content:center;
+	align-items:center;
+	height:100vh;
+	    }
+
+.container {
+	background-color: #fff;
+	padding:30px;
+	border-radius:10px;
+	box-shadow:0px 0px 10px rgba(0,0,0,0.2);
+	text-align:center;
+	}
+
+h1{
+	font-size:24px;
+	color:purple;
+	
+}
+
+.form-group {
+	margin: 20px 0;
+	   }
+
+label {
+	font-size:20px;
+	font-weight:bold;
+	color:red;
+	}
+
+input[type="number"]
+{
+width: 250px;
+padding: 10px;
+border: 1px solid black;
+border-radius:4px;
+}
+
+button {
+	background: #007bff;
+	color:white;
+	border:none;
+	padding: 10px 20px;
+	border-radius: 5px;
+	cursor:pointer;
+}
+
+button:hover {
+	background:black;
+}
+	
